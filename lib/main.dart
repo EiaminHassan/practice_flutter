e3304c8a7a24df6019f02fc0d9f8f45351166f5f
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-03-10 23:38:51 +0600

    add_net_image

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,9 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.orange,
         ),
         body: Center(
-          child: Text(
-            "middle of the body",
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.blueGrey,
+          child: Image(
+            image: NetworkImage(
+              "https://images.unsplash.com/photo-1598356918644-7a5af992f40c?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             ),
           ),
         ),
