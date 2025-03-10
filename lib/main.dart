522589346bd0e9e00b430ffb380ce66fd45d2f4c
EiaminHassan <shanto15-5251@diu.edu.bd>
2025-03-10 23:47:39 +0600

    assets_image_add

--- lib/main.dart
+++ lib/main.dart
@@ -16,13 +16,7 @@ class MyApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.orange,
         ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-              "https://images.unsplash.com/photo-1598356918644-7a5af992f40c?q=80&w=1948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            ),
-          ),
-        ),
+        body: Center(child: Image(image: AssetImage("assets/spider.jpg"))),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           backgroundColor: Colors.amber,
